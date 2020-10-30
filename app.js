88a5d861e66c5bae9add85c35018d5b58b92c372
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2020-10-29 21:48:15 -0400

    Revised email validation funciton console.log spacing

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ function continueTeam() {
 const validateEmail = (email) => {
     valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
     if (valid) {
-        console.log("Great job");
+        console.log(" - Email validated");
         return true;
     } else {
         console.log(".  Please enter a valid email")
